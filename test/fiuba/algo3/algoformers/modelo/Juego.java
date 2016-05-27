53b1987470a03c56bb6fc23f8faf091108a7c13e
Rodrigo <rodrigoolivera27@yahoo.com.ar>
2016-05-27 20:12:20 -0300

    "Agregado archivo supuestos"

---
+++ test/fiuba/algo3/algoformers/modelo/Juego.java
@@ -0,0 +1,24 @@
+package fiuba.algo3.algoformers.modelo;
+
+import org.junit.Test;
+
+/**
+ * Created by Rodrigo on 27/05/2016.
+ */
+public class Juego {
+
+    @Test
+    public void testGeneric(){
+
+    }
+
+    @Test
+    public void testCrearNuevoJuego(){
+
+
+    }
+
+
+
+
+}
