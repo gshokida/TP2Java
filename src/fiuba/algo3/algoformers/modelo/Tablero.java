7fdafa3af009a708cce652ff9f47b6d9a9115a03
Rodrigo <rodrigoolivera27@yahoo.com.ar>
2016-05-27 21:43:34 -0300

    "Agregados Tablero y TableroTest, faltan algunas cosas como agregar todos los casilleros al tablero"

---
+++ src/fiuba/algo3/algoformers/modelo/Tablero.java
@@ -0,0 +1,55 @@
+package fiuba.algo3.algoformers.modelo;
+
+/**
+ * Created by Rodrigo on 27/05/2016.
+ */
+public class Tablero {
+
+    Casillero tablero [] [];
+    int posx;
+    int posy;
+    int posxElementoMedio;
+    int posyElementoMedio;
+
+    public  Tablero (int x, int y){
+
+        this.tablero = new Casillero[x][y];
+        this.posx = x;
+        this.posy = y;
+        this.posxElementoMedio = x/2;
+        this.posyElementoMedio = y/2;
+
+    }
+
+
+    public boolean tieneTamanio(int i) {
+
+        return ((this.posx * this.posy)== i);
+
+    }
+
+    public int medio() {
+
+        return (this.posxElementoMedio * this.posyElementoMedio);
+
+    }
+
+
+    public Casillero devolverElementoMedio (){
+
+        return (this.tablero [this.posxElementoMedio] [this.posyElementoMedio]);
+
+    }
+
+    //METODO SOLO PARA TEST
+
+    public void llenarCasilleroDelMedio ( Casillero casillero) {
+
+        this.tablero [this.posxElementoMedio] [this.posyElementoMedio] = casillero;
+
+    }
+
+    //Habria que inicializar todos los casilleros del tablero.
+
+
+}
---
+++ test/fiuba/algo3/algoformers/modelo/TableroTest.java
@@ -0,0 +1,63 @@
+package fiuba.algo3.algoformers.modelo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Rodrigo on 27/05/2016.
+ */
+public class TableroTest {
+
+
+    @Test
+    public void testGeneric(){
+
+    }
+
+
+    //NOTA MENTAL: Los tableros deben tener numeros pares porque los arrays empiezan en el elento 0.
+
+    @Test
+    public void testCrearTablero(){
+
+        Tablero tablero = new Tablero(4,4);
+
+        Assert.assertTrue(tablero.tieneTamanio(4*4));
+
+
+    }
+
+    @Test
+    public void testBuscarMedio(){
+
+        Tablero tablero = new Tablero(4,4);
+
+        Assert.assertTrue(tablero.medio() == 4);
+
+    }
+
+    @Test
+    public void testEncontrarElElementoMedioDeCasillero(){
+
+        Tablero tablero = new Tablero(4,4);
+
+        Casillero casillero = new Casillero();
+
+        tablero.llenarCasilleroDelMedio(casillero);
+
+        Assert.assertTrue(tablero.devolverElementoMedio().equals(casillero));
+
+    }
+
+
+
+
+
+    @Test
+    public void testUnTableroImparLanzaExcepcion(){
+
+    }
+
+
+
+}
