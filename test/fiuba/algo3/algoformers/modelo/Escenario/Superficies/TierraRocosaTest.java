6e2e829fa50086a3b8de3749b645ed28a6d42dcb
gshokida <german.shokida@gmail.com>
2016-06-06 11:20:39 -0300

    Interaccion Optimus transformado con Tierra Rocosa

--- test/fiuba/algo3/algoformers/modelo/Escenario/Superficies/TierraRocosaTest.java
+++ test/fiuba/algo3/algoformers/modelo/Escenario/Superficies/TierraRocosaTest.java
@@ -1,5 +1,6 @@
 package fiuba.algo3.algoformers.modelo.Escenario.Superficies;
 
+import fiuba.algo3.algoformers.modelo.ManejoDeJuego.Acciones.Transformacion;
 import fiuba.algo3.algoformers.modelo.Personajes.AlgoFormer;
 import fiuba.algo3.algoformers.modelo.Personajes.AlgoFormers.Optimus;
 import org.junit.Test;
@@ -21,5 +22,21 @@ public class TierraRocosaTest {
         assertEquals(50, optimus.getAtaque());
         assertEquals(2, optimus.getDistanciaDeAtaque());
         assertEquals(2, optimus.getVelocidad());
+    }
+
+    @Test
+    public void superficieRocosaNoAplicaNingunEfectoSobreOptimusTransformado(){
+        AlgoFormer optimus = new Optimus();
+        TierraRocosa tierraRocosa = new TierraRocosa();
+        Transformacion transformacion = new Transformacion(optimus);
+
+        transformacion.aplicarTransformacion();
+
+        tierraRocosa.interactuar(optimus);
+
+        assertEquals(500, optimus.getPuntosDeVida());
+        assertEquals(15, optimus.getAtaque());
+        assertEquals(4, optimus.getDistanciaDeAtaque());
+        assertEquals(5, optimus.getVelocidad());
     }
 }
