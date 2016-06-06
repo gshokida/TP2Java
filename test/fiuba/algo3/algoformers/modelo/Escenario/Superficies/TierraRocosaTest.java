ae19b3b458cae70024302bc3c666e23fe233f62e
gshokida <german.shokida@gmail.com>
2016-06-06 11:23:57 -0300

    Interaccion con Bumblebee

--- test/fiuba/algo3/algoformers/modelo/Escenario/Superficies/TierraRocosaTest.java
+++ test/fiuba/algo3/algoformers/modelo/Escenario/Superficies/TierraRocosaTest.java
@@ -2,6 +2,7 @@ package fiuba.algo3.algoformers.modelo.Escenario.Superficies;
 
 import fiuba.algo3.algoformers.modelo.ManejoDeJuego.Acciones.Transformacion;
 import fiuba.algo3.algoformers.modelo.Personajes.AlgoFormer;
+import fiuba.algo3.algoformers.modelo.Personajes.AlgoFormers.Bumblebee;
 import fiuba.algo3.algoformers.modelo.Personajes.AlgoFormers.Optimus;
 import org.junit.Test;
 
@@ -38,5 +39,34 @@ public class TierraRocosaTest {
         assertEquals(15, optimus.getAtaque());
         assertEquals(4, optimus.getDistanciaDeAtaque());
         assertEquals(5, optimus.getVelocidad());
+    }
+
+    @Test
+    public void superficieRocosaNoAplicaNingunEfectoSobreBumblebee(){
+        AlgoFormer bumblebee = new Bumblebee();
+        TierraRocosa tierraRocosa = new TierraRocosa();
+
+        tierraRocosa.interactuar(bumblebee);
+
+        assertEquals(350, bumblebee.getPuntosDeVida());
+        assertEquals(40, bumblebee.getAtaque());
+        assertEquals(1, bumblebee.getDistanciaDeAtaque());
+        assertEquals(2, bumblebee.getVelocidad());
+    }
+
+    @Test
+    public void superficieRocosaNoAplicaNingunEfectoSobreBumblebeeTransformado(){
+        AlgoFormer bumblebee = new Bumblebee();
+        TierraRocosa tierraRocosa = new TierraRocosa();
+        Transformacion transformacion = new Transformacion(bumblebee);
+
+        transformacion.aplicarTransformacion();
+
+        tierraRocosa.interactuar(bumblebee);
+
+        assertEquals(350, bumblebee.getPuntosDeVida());
+        assertEquals(20, bumblebee.getAtaque());
+        assertEquals(3, bumblebee.getDistanciaDeAtaque());
+        assertEquals(5, bumblebee.getVelocidad());
     }
 }
