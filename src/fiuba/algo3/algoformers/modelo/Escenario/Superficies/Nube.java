5b3c326c380e07bd11ba009f6824a61d306005b8
gshokida <german.shokida@gmail.com>
2016-06-06 14:20:36 -0300

    Interaccion de Nube con Tipo unidades Aereas

--- src/fiuba/algo3/algoformers/modelo/Escenario/Superficies/Nube.java
+++ src/fiuba/algo3/algoformers/modelo/Escenario/Superficies/Nube.java
@@ -1,19 +1,17 @@
 package fiuba.algo3.algoformers.modelo.Escenario.Superficies;
 
+import fiuba.algo3.algoformers.modelo.Errores.NoPuedeInteractuarConSuperficieException;
+import fiuba.algo3.algoformers.modelo.Personajes.AlgoFormer;
+import fiuba.algo3.algoformers.modelo.Personajes.TiposDeUnidades.TipoUnidadAerea;
+import fiuba.algo3.algoformers.modelo.Personajes.TiposDeUnidades.TipoUnidadTerrestre;
+
 /**
  * Created by german.shokida on 27/5/2016.
  */
 public class Nube implements SuperficieAerea {
-    private static SuperficieAerea nube;
 
-    private Nube() {
-
-    }
-
-    public static SuperficieAerea getInstance() {
-        if (Nube.nube == null)
-            Nube.nube = new Nube();
-
-        return Nube.nube;
+    public void interactuar(AlgoFormer algoformer) throws NoPuedeInteractuarConSuperficieException {
+        if(algoformer.esTipoUnidad(TipoUnidadTerrestre.getInstance()))
+            throw new NoPuedeInteractuarConSuperficieException();
     }
 }
